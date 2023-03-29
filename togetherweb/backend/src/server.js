8a42b36892267efa267583ed3d3fec84fbe12e45
28sanduni <99sanduni99@gmail.com>
2023-03-29 17:52:00 +0530

    nodeJS server created

---
+++ togetherweb/backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:4200']
+}));
+
+app.get("/",(req,res) => {
+    res.send("hello world");
+})
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
+})
