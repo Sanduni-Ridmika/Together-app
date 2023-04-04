684f5e71455be2c088c4e5cb5e8f7ab2a1fddc63
28sanduni <99sanduni99@gmail.com>
2023-04-04 20:42:23 +0530

    questionnaire page front

--- TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
@@ -1,11 +1,20 @@
 package com.example.togetherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Information : AppCompatActivity() {
+    private lateinit var button1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
+
+        button1 = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, InfoDisplay::class.java)
+            startActivity(intent)
+        }
     }
 }
