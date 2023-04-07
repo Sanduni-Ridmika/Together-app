40276a2100f5d9d4930ab983167c965bf2dcf398
28sanduni <99sanduni99@gmail.com>
2023-04-07 20:39:56 +0530

    information display pages

--- TogetherApp/app/src/main/java/com/example/togetherapp/InfoDisplay.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/InfoDisplay.kt
@@ -2,10 +2,27 @@ package com.example.togetherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class InfoDisplay : AppCompatActivity() {
+    private lateinit var question_text: TextView
+    private lateinit var detail_text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_display)
+
+        question_text = findViewById(R.id.question_text)
+        detail_text = findViewById(R.id.detail_text)
+
+        // Get the text passed from Information page
+        val question = intent.getStringExtra("question")
+        val detail = intent.getStringExtra("detail")
+
+        // Update TextViews with appropriate text
+        question_text.text = question
+        detail_text.text = detail
+
     }
 }
--- TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
@@ -7,13 +7,63 @@ import android.widget.Button
 
 class Information : AppCompatActivity() {
     private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
 
+        // Set up click listener for button1
         button1 = findViewById(R.id.button1)
         button1.setOnClickListener {
             val intent = Intent(this, InfoDisplay::class.java)
+            intent.putExtra("question", "What is Depression?")
+            intent.putExtra("detail", "Depression is a mental health disorder that is characterized by persistent feelings of sadness and a loss of interest or pleasure in activities that a person used to enjoy. It is a serious condition that can affect a person's daily life and functioning, and it often requires treatment by a mental health professional.")
+            startActivity(intent)
+        }
+
+        // Set up click listener for button2
+        button2 = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, InfoDisplay::class.java)
+            intent.putExtra("question", "Symptoms of Depression")
+            intent.putExtra("detail", "• Persistent feelings of sadness, emptiness, or hopelessness\n" +
+                    "• Loss of interest or pleasure in activities that used to be enjoyable\n" +
+                    "• Fatigue or lack of energy\n" +
+                    "• Changes in appetite or weight\n" +
+                    "• Difficulty sleeping or sleeping too much\n" +
+                    "• Restlessness or irritability\n" +
+                    "• Difficulty concentrating or making decisions\n" +
+                    "• Feelings of worthlessness or guilt\n" +
+                    "• Thoughts of suicide or self-harm\n")
+            startActivity(intent)
+        }
+
+        // Set up click listener for button3
+        button3 = findViewById(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, InfoDisplay::class.java)
+            intent.putExtra("question", "Treatments for Depression")
+            intent.putExtra("detail", "• Talk therapy with a mental health professional\n" +
+                    "• Medications prescribed by a doctor or psychiatrist\n" +
+                    "• Exercise or physical activity\n" +
+                    "• Mindfulness-based practices like meditation or yoga\n" +
+                    "• Lifestyle changes such as improving sleep habits and reducing stress\n")
+            startActivity(intent)
+        }
+
+        // Set up click listener for button4
+        button4 = findViewById(R.id.button4)
+        button4.setOnClickListener {
+            val intent = Intent(this, InfoDisplay::class.java)
+            intent.putExtra("question", "Local Support Centers")
+            intent.putExtra("detail", "• National Institute of Mental Health - +94 11 2695295\n" +
+                    "• Sumithrayo - +94 11 2696666\n" +
+                    "• CCC Foundation - +94 77 774 4242\n" +
+                    "• Institute for Psychological Health - +94 77 384 4224\n" +
+                    "• National Council for Mental Health - +94 11 288 6786\n")
             startActivity(intent)
         }
     }
