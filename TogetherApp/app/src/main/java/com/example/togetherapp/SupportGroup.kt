c15110723251752ded02d26826e60bdb69531281
28sanduni <99sanduni99@gmail.com>
2023-04-16 03:01:46 +0530

    user profile page initial commit

--- TogetherApp/app/src/main/java/com/example/togetherapp/SupportGroup.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/SupportGroup.kt
@@ -1,11 +1,22 @@
 package com.example.togetherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SupportGroup : AppCompatActivity() {
+    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support_group)
+
+        text = findViewById(R.id.text)
+        text.setOnClickListener {
+            val intent = Intent(this, UserProfile::class.java)
+            startActivity(intent)
+        }
     }
 }
