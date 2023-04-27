9f3461882ce7f1a5a5cedfab1284c09ef20ce3a5
28sanduni <99sanduni99@gmail.com>
2023-04-27 14:21:26 +0530

    hamburger button

--- TogetherApp/app/src/main/java/com/example/togetherapp/Home.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/Home.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.PopupMenu
+import com.google.firebase.auth.FirebaseAuth
 
 class Home : AppCompatActivity() {
     private lateinit var button1: Button
@@ -43,40 +44,39 @@ class Home : AppCompatActivity() {
             val intent = Intent(this, SupportGroup::class.java)
             startActivity(intent)
         }
-
+        // Find and setup the hamburger button
         hamburgerButton = findViewById(R.id.hamburgerButton)
-        hamburgerButton.setOnClickListener {
-            // Handle hamburgerButton click
-            // showPopupMenu()
-        }
+        hamburgerButton.setOnClickListener { showPopupMenu() }
     }
-    /* private fun showPopupMenu() {
-        // Create a PopupMenu for overflow options
-         val popupMenu = PopupMenu(this, hamburgerButton)
-         popupMenu.menuInflater.inflate(R.menu.overflow_menu, popupMenu.menu)
-          popupMenu.setOnMenuItemClickListener { item: MenuItem ->
-             when (item.itemId) {
-                 R.id.menu_profile -> {
-                     // Handle Profile option
-                     // Start the ProfileActivity
-                     val intent = Intent(this, UserProfile::class.java)
-                     startActivity(intent)
-                     true
-                 }
-                 R.id.menu_logout -> {
-                     // Handle Logout option
-                     // Perform logout operation
-                     // For example: clear session, remove tokens, etc.
-                     // Then start the LoginActivity
-                     val intent = Intent(this, LogIn ::class.java)
-                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                     startActivity(intent)
-                     finish()
-                     true
-                 }
-                 else -> false
-             }
-         }
-         popupMenu.show()
-     } */
+    // Function to show the popup menu
+    private fun showPopupMenu() {
+        val popupMenu = PopupMenu(this, hamburgerButton)
+        popupMenu.menuInflater.inflate(R.menu.hamburger_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_profile -> {
+                    // Open the profile activity
+                    val intent = Intent(this, UserProfile::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_logout -> {
+                    // Perform logout action
+                    performLogout()
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show()
+    }
+
+    // Function to perform the logout action
+    private fun performLogout() {
+        // Log out the user
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, LogIn::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
