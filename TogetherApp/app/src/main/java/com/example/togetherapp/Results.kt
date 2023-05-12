5b49d99d6d9e1617d3f5956bf9645b7c6661f12f
28sanduni <99sanduni99@gmail.com>
2023-05-12 22:12:07 +0530

    better UI result page

--- TogetherApp/app/src/main/java/com/example/togetherapp/Results.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/Results.kt
@@ -2,9 +2,17 @@ package com.example.togetherapp
 
 import BaseActivity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class Results : BaseActivity() {
@@ -15,6 +23,7 @@ class Results : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
 
+        val stressMeter = findViewById<ImageView>(R.id.scale)
 
         btnExercise = findViewById(R.id.btnExercise)
         btnExercise.setOnClickListener {
@@ -27,16 +36,35 @@ class Results : BaseActivity() {
         // Get the prediction from the intent extra
         val prediction = intent.getStringExtra("prediction")
 
-        // Display the prediction in the text1 TextView
-        text1.text = "$prediction"
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val colorSpan = ForegroundColorSpan(Color.parseColor("#b30000"))
+        val sizeSpan = AbsoluteSizeSpan(18, true)
 
+        // Display the prediction in the TextView
         if(prediction=="Not Depressed"){
-            text2.text="Not Depressed"
-        }else if(prediction=="Depressed"){
-            text2.text="Depressed"
-        }else if(prediction=="Severely Depressed"){
-            text2.text="Severely Depressed"
+            text1.text= "Congratulations!\n\n" + "you're not suffering from Depression."
+            text2.text="The responses you have given indicate that you're not suffering from depression."
+            stressMeter.setImageResource(R.drawable.scale1)
         }
 
+        else if(prediction=="Depressed"){
+            val spannable = SpannableString("These results indicate that you’re suffering from\n\nDepression")
+            spannable.setSpan(boldSpan, 51, 61, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spannable.setSpan(colorSpan, 51, 61, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spannable.setSpan(sizeSpan, 51, 61, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            text1.text = spannable
+            text2.text= "Note that these results are not meant to be a diagnosis. Therefore, we would always suggest you to seek for professional help and support."
+            stressMeter.setImageResource(R.drawable.scale2)
+        }
+
+        else if(prediction=="Severely Depressed"){
+            val spannable = SpannableString("These results indicate that you’re suffering from\n\nSevere Depression")
+            spannable.setSpan(boldSpan, 51, 68, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spannable.setSpan(colorSpan, 51, 68, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spannable.setSpan(sizeSpan, 51, 68, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            text1.text = spannable
+            text2.text="Note that these results are not meant to be a diagnosis. Therefore, we would always suggest you to seek for professional help and support."
+            stressMeter.setImageResource(R.drawable.scale3)
+        }
     }
 }
