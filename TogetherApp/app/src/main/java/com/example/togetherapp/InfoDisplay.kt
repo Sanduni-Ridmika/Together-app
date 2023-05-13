c410aec6faac579f86791837703b89509bf54d99
28sanduni <99sanduni99@gmail.com>
2023-05-13 17:23:21 +0530

    information pages fixes

--- TogetherApp/app/src/main/java/com/example/togetherapp/InfoDisplay.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/InfoDisplay.kt
@@ -1,10 +1,17 @@
 package com.example.togetherapp
 
 import BaseActivity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.util.regex.Pattern
 
 class InfoDisplay : BaseActivity() {
     private lateinit var question_text: TextView
@@ -19,11 +26,26 @@ class InfoDisplay : BaseActivity() {
 
         // Get the text passed from Information page
         val question = intent.getStringExtra("question")
-        val detail = intent.getStringExtra("detail")
+        var detail = intent.getStringExtra("detail")
+
+        // Add bullet list HTML to the detail text, except for the first line in button1
+        if (question != "What is Depression?") {
+            detail = detail?.let { "<ul><li>&nbsp;${it.replace("\n", "</li><li>&nbsp;")}</li></ul>" }
+        }
 
         // Update TextViews with appropriate text
         question_text.text = question
-        detail_text.text = detail
+        detail_text.text = fromHtml(detail ?: "")
+
+    }
+
+    @Suppress("DEPRECATION")
+    private fun fromHtml(html: String): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(html)
+        }
 
     }
 }
--- TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
+++ TogetherApp/app/src/main/java/com/example/togetherapp/Information.kt
@@ -2,8 +2,14 @@ package com.example.togetherapp
 
 import BaseActivity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BulletSpan
+import android.text.style.StyleSpan
 import android.widget.Button
 
 class Information : BaseActivity() {
@@ -19,38 +25,44 @@ class Information : BaseActivity() {
         // Set up click listener for button1
         button1 = findViewById(R.id.button1)
         button1.setOnClickListener {
+            val causes = " A combination of genetic, environmental, and psychological factors\n" +
+                    " A family history of depression\n" +
+                    " Significant life changes or stressful events\n" +
+                    " Chemical imbalances in the brain\n" +
+                    " Hormonal changes, such as those during pregnancy or menopause\n" +
+                    " Substance abuse or addiction\n" +
+                    " Social isolation or loneliness\n"+
+                    " Poor nutrition and lack of physical exercise\n"+
+                    " Low self-esteem or negative self-talk\n" +
+                    " Certain medications or medical treatments\n" +
+                    " Personality traits such as perfectionism or pessimism."
+
+            // Convert causes text to bullet list HTML
+            val bulletListHTML = "<ul><li>&nbsp;${causes.replace("\n", "</li><li>&nbsp;")}</li></ul>"
+
             val intent = Intent(this, InfoDisplay::class.java)
             intent.putExtra("question", "What is Depression?")
             intent.putExtra("detail", "Depression is a mental health disorder characterized by persistent feelings of sadness, hopelessness, and a lack of interest or pleasure in everyday activities.\n" + "It can also involve physical symptoms such as fatigue, changes in appetite or sleep patterns, and difficulty concentrating.\n\n" +
-                    getString(R.string.depression_causes) +
-                    "• A combination of genetic, environmental, and psychological factors\n" +
-                    "• A family history of depression\n" +
-                    "• Significant life changes or stressful events\n" +
-                    "• Chemical imbalances in the brain\n" +
-                    "• Hormonal changes, such as those during pregnancy or menopause\n" +
-                    "• Substance abuse or addiction\n" +
-                    "• Social isolation or loneliness\n"+
-                    "• Poor nutrition and lack of physical exercise\n"+
-                    "• Low self-esteem or negative self-talk\n" +
-                    "• Certain medications or medical treatments\n" +
-                    "• Personality traits such as perfectionism or pessimism.")
+                    "<br> <br> <b> Causes of Depression,</b>" +  bulletListHTML)
             startActivity(intent)
+
         }
+
 
         // Set up click listener for button2
         button2 = findViewById(R.id.button2)
         button2.setOnClickListener {
             val intent = Intent(this, InfoDisplay::class.java)
             intent.putExtra("question", "Symptoms of Depression")
-            intent.putExtra("detail", "• Persistent feelings of sadness, emptiness, or hopelessness\n" +
-                    "• Loss of interest or pleasure in activities that used to be enjoyable\n" +
-                    "• Fatigue or lack of energy\n" +
-                    "• Changes in appetite or weight\n" +
-                    "• Difficulty sleeping or sleeping too much\n" +
-                    "• Restlessness or irritability\n" +
-                    "• Difficulty concentrating or making decisions\n" +
-                    "• Feelings of worthlessness or guilt\n" +
-                    "• Thoughts of suicide or self-harm\n")
+            intent.putExtra("detail", " Persistent feelings of sadness, emptiness, or hopelessness\n" +
+                    " Loss of interest or pleasure in activities that used to be enjoyable\n" +
+                    " Fatigue or lack of energy\n" +
+                    " Changes in appetite or weight\n" +
+                    " Difficulty sleeping or sleeping too much\n" +
+                    " Restlessness or irritability\n" +
+                    " Difficulty concentrating or making decisions\n" +
+                    " Feelings of worthlessness or guilt\n" +
+                    " Thoughts of suicide or self-harm")
             startActivity(intent)
         }
 
@@ -59,13 +71,13 @@ class Information : BaseActivity() {
         button3.setOnClickListener {
             val intent = Intent(this, InfoDisplay::class.java)
             intent.putExtra("question", "Treatments for Depression")
-            intent.putExtra("detail", "• Talk therapy with a mental health professional\n" +
-                    "• Medications prescribed by a doctor or psychiatrist\n" +
-                    "• Exercise or physical activity\n" +
-                    "• Mindfulness-based practices like meditation or yoga\n" +
-                    "• Listening to mind relaxing music\n" +
-                    "• Mind relaxing exercises such as breathing exercises\n" +
-                    "• Lifestyle changes such as improving sleep habits and reducing stress\n")
+            intent.putExtra("detail", " Talk therapy with a mental health professional\n" +
+                    " Medications prescribed by a doctor or psychiatrist\n" +
+                    " Exercise or physical activity\n" +
+                    " Mindfulness-based practices like meditation or yoga\n" +
+                    " Listening to mind relaxing music\n" +
+                    " Mind relaxing exercises such as breathing exercises\n" +
+                    " Lifestyle changes such as improving sleep habits and reducing stress")
             startActivity(intent)
         }
 
@@ -74,11 +86,11 @@ class Information : BaseActivity() {
         button4.setOnClickListener {
             val intent = Intent(this, InfoDisplay::class.java)
             intent.putExtra("question", "Local Support Centers")
-            intent.putExtra("detail", "• National Institute of Mental Health - +94 11 2695295\n" +
-                    "• Sumithrayo - +94 11 2696666\n" +
-                    "• CCC Foundation - +94 77 774 4242\n" +
-                    "• Institute for Psychological Health - +94 77 384 4224\n" +
-                    "• National Council for Mental Health - +94 11 288 6786\n")
+            intent.putExtra("detail", "National Institute of Mental Health <br><a href='tel:0112695295'>011 269 5295</a>\n" +
+                    "Sumithrayo <br><a href='tel:0112696666'>011 269 6666</a>\n" +
+                    "CCC Foundation <br><a href='tel:0777744242'>077 774 4242</a>\n" +
+                    "Institute for Psychological Health <br><a href='tel:0773844224'>077 384 4224</a>\n" +
+                    "National Council for Mental Health <br><a href='tel:0112886786'>011 288 6786</a>")
             startActivity(intent)
         }
     }
